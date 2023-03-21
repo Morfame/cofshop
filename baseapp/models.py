4a743ee9592c1ed3db1653734ac83d2f8c307b33
Steve Morris <baramorf.com>
2023-03-21 09:46:55 +0000

    database creation  started

--- baseapp/models.py
+++ baseapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Shop(models.Model):
+    #host
+    #menu
+    name = models.CharField(max_length=100)
+    desc = models.TextField(max_length=300, null=True, blank=True)
+    covers = models.IntegerField(default=1)
+    date_opened = models.DateField()
+    date_closed = models.DateField(null=True, blank=True)
+    location = models.CharField(max_length=200)
+    manager = models.CharField(max_length=50)
+    tel_main = models.CharField(max_length=50, null=True, blank=True)
+    tel_man = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    
