8636de29bf5532cae156a2b97922d8d40081fa17
Steve Morris <baramorf.com>
2023-04-02 11:04:17 +0100

    Python Anywhere Static root

--- cofshop/settings.py
+++ cofshop/settings.py
@@ -105,6 +105,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = "/home/Morfame/cofshop/static"
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
